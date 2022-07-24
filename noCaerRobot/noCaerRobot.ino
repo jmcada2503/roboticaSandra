27a11447a1b23680017f32e1407c0694104bd38a
Juan Miguel Cadavid Jiménez <jucadavidj@unal.edu.co>
2022-07-24 10:57:09 -0500

    robot no caer

---
+++ noCaerRobot/noCaerRobot.ino
@@ -0,0 +1,28 @@
+#define leftMotor M3
+#define rightMotor M1
+#define leftSensor J2
+#define rightSensor J1
+
+void setup() {
+  // put your setup code here, to run once:
+  motorOn(leftMotor, FORWARD);
+  motorOn(rightMotor, FORWARD);
+  motorSpeed(M1, 60);
+  motorSpeed(M2, 60);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("leftSensor: ");
+  Serial.print(lineRead(leftSensor));
+  Serial.print("    rightSensor: ");
+  Serial.println(lineRead(rightSensor));
+  goForward(leftMotor, rightMotor);
+  if (lineRead(leftSensor) == 0 || lineRead(rightSensor) == 0) {
+    goReverse(leftMotor, rightMotor);
+    delay(500);
+    turnRight(leftMotor, rightMotor);
+    delay(300);
+  }
+}
